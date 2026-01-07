3efe11cc07f16f0ba097a1794c39e732a1b14c68
Zain Raza <100517579+Zain-128@users.noreply.github.com>
2026-01-08 01:52:39 +0500

    feat:is First time asyncstorage.

--- src/screens/getStarted/GetStarted.tsx
+++ src/screens/getStarted/GetStarted.tsx
@@ -5,12 +5,25 @@ import GradientButton from '../../components/gradientButton/GradientButton';
 import colors from '../../config/colors';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import fonts from '../../config/fonts';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const GetStarted = ({ navigation }: { navigation: any }) => {
   const { bottom } = useSafeAreaInsets();
   const style = styles(bottom);
-  const onGetStartedPress = () => {
-    navigation.navigate('Intro');
+  const onGetStartedPress = async () => {
+
+    try {
+      const isFirstTime = await AsyncStorage.getItem('isFirstTime');
+
+      if (isFirstTime === 'false' ) {
+        navigation.navigate('Login');
+      } else {
+        await AsyncStorage.setItem('isFirstTime', 'false');
+        navigation.navigate('Intro');
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
   };
   return (
     <View style={style.container}>
